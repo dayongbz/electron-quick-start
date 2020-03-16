52878747ce97c36a5d9f300ca4e9d9b311c39c51
dayongbz <dayongbz@gmail.com>
2020-03-16 22:20:14 +0900

    fix cmd+w

--- main.js
+++ main.js
@@ -1,41 +1,92 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Tray } = require("electron");
+const path = require("path");
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+let mainWindow = null;
+let tray = null;
+let willQuitApp = false;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 320,
+    height: 450,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: true
+  });
+
+  mainWindow.loadURL("https://vibe.naver.com/");
+
+  mainWindow.on("close", e => {
+    if (willQuitApp) {
+      mainWindow = null;
+    } else {
+      e.preventDefault();
+      mainWindow.hide();
     }
-  })
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
+  });
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+function getWindowPosition() {
+  const windowBounds = mainWindow.getBounds();
+  const trayBounds = tray.getBounds();
+
+  // Center window horizontally below the tray icon
+  const x = Math.round(
+    trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
+  );
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y + trayBounds.height + 4);
+  return { x, y };
+}
+
+function showWindow() {
+  const position = getWindowPosition();
+  mainWindow.setPosition(position.x, position.y, false);
+  mainWindow.show();
+}
+
+function toggleWindow(e) {
+  try {
+    mainWindow.isVisible() ? mainWindow.hide() : showWindow();
+  } catch (error) {
+    createTray();
+  }
+}
+
+function createTray() {
+  tray = new Tray(path.join(__dirname, "vibe_appicon@1.5x.png"));
+  tray.on("click", toggleWindow);
+}
+
+app.dock.hide();
+
+app.on("ready", () => {
+  createWindow();
+  createTray();
+});
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function() {
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('activate', function () {
+app.on("activate", function() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+app.on("browser-window-blur", () => {
+  mainWindow.hide();
+});
+
+app.on("before-quit", () => {
+  willQuitApp = true;
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
